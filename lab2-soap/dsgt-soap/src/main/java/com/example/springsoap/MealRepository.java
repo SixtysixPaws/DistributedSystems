7b89429146a28f8587ce8f6ef6c1cd02f315a20f
jiatianguo <jiatian.guo@student.kuleuven.be>
2026-03-20 12:03:00 +0100

    Complete Lab 2 SOAP: implemented cheapest meal, add order, and get meals by type

--- lab2-soap/dsgt-soap/src/main/java/com/example/springsoap/MealRepository.java
+++ lab2-soap/dsgt-soap/src/main/java/com/example/springsoap/MealRepository.java
@@ -1,10 +1,7 @@
 package com.example.springsoap;
 
 import javax.annotation.PostConstruct;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.NoSuchElementException;
+import java.util.*;
 
 
 import io.foodmenu.gt.webservice.*;
@@ -25,6 +22,7 @@ public class MealRepository {
         a.setDescription("Steak with fries");
         a.setMealtype(Mealtype.MEAT);
         a.setKcal(1100);
+        a.setPrice(25.50);
 
 
         meals.put(a.getName(), a);
@@ -34,7 +32,7 @@ public class MealRepository {
         b.setDescription("Portobello Mushroom Burger");
         b.setMealtype(Mealtype.VEGAN);
         b.setKcal(637);
-
+        b.setPrice(15.0);
 
         meals.put(b.getName(), b);
 
@@ -43,6 +41,7 @@ public class MealRepository {
         c.setDescription("Fried fish with chips");
         c.setMealtype(Mealtype.FISH);
         c.setKcal(950);
+        c.setPrice(22.0);
 
 
         meals.put(c.getName(), c);
@@ -63,5 +62,24 @@ public class MealRepository {
 
     }
 
+    public Meal findCheapestMeal() {
+        return meals.values().stream()
+                .min(java.util.Comparator.comparing(Meal::getPrice))
+                .orElseThrow(java.util.NoSuchElementException::new);
+    }
 
+    // 在类中添加处理订单的方法
+    public OrderConfirmation processOrder(Order order) {
+        OrderConfirmation confirmation = new OrderConfirmation();
+        // 使用 UUID 生成一个唯一的订单号
+        confirmation.setId(java.util.UUID.randomUUID().toString());
+        confirmation.setStatus("Order received for address: " + order.getAddress());
+        return confirmation;
+    }
+
+    public List<Meal> findByMealType(Mealtype type) {
+        return meals.values().stream()
+                .filter(meal -> meal.getMealtype().equals(type))
+                .collect(java.util.stream.Collectors.toList());
+    }
 }
--- lab2-soap/dsgt-soap/src/main/java/com/example/springsoap/MenuEndpoint.java
+++ lab2-soap/dsgt-soap/src/main/java/com/example/springsoap/MenuEndpoint.java
@@ -39,5 +39,29 @@ public class MenuEndpoint {
         return response;
     }
 
+    @PayloadRoot(namespace = "http://foodmenu.io/gt/webservice", localPart = "getCheapestMealRequest")
+    @ResponsePayload
+    public GetCheapestMealResponse getCheapestMeal(@RequestPayload GetCheapestMealRequest request) {
+        GetCheapestMealResponse response = new GetCheapestMealResponse();
+        response.setMeal(mealrepo.findCheapestMeal());
+        return response;
+    }
+
+    @PayloadRoot(namespace = "http://foodmenu.io/gt/webservice", localPart = "addOrderRequest")
+    @ResponsePayload
+    public AddOrderResponse addOrder(@RequestPayload AddOrderRequest request) {
+        AddOrderResponse response = new AddOrderResponse();
+        // 调用 repository 处理订单
+        response.setOrderConfirmation(mealrepo.processOrder(request.getOrder()));
+        return response;
+    }
+
+    @PayloadRoot(namespace = "http://foodmenu.io/gt/webservice", localPart = "getMealsByTypeRequest")
+    @ResponsePayload
+    public GetMealsByTypeResponse getMealsByType(@RequestPayload GetMealsByTypeRequest request) {
+        GetMealsByTypeResponse response = new GetMealsByTypeResponse();
+        response.getMeals().addAll(mealrepo.findByMealType(request.getMealtype()));
+        return response;
+    }
 
 }
